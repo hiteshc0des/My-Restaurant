74d8054477f567ff9621fb29da0a68648246e964
hiidev12 <hiteshmaurya213@gmail.com>
2024-08-30 10:40:52 +0530

    second commit

--- src/components/MenuButtons.jsx
+++ src/components/MenuButtons.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import MenuData from "./MenuData";
+// import "./MenuButton.css";
 
 const MenuButtons = ({ menuItems, filterItems, setItems }) => {
   return (
-    <div className="d-flex justify-content-center mb-5 mt-5">
+    <div className="d-flex flex-wrap justify-content-center mb-5 mt-5">
+      <button
+        className="d-flex flex-wrap justify-content-center mb-2 mt-2 "
+        onClick={() => setItems(MenuData)}
+        style={{
+          gap: "1rem",
+          padding: "0.3rem 0.8rem",
+          flexDirection: "column",
+          width: "93%",
+          alignItems: "center",
+        }}
+      >
+        All
+      </button>
       {menuItems.map((val) => (
         <button
-          className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
+          className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold btn-sm"
           onClick={() => filterItems(val)}
+          style={{
+            gap: "1rem",
+            padding: "0.3rem 0.8rem",
+            flexDirection: "column",
+            width: "100%",
+            margin: "2px",
+          }}
         >
           {val}
         </button>
       ))}
-      <button
-        className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
-        onClick={() => setItems(MenuData)}
-      >
-        All
-      </button>
     </div>
   );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,10 @@ import AboutChef1 from "../utils/img/about-chef1.jpg";
 import AboutChef2 from "../utils/img/about-chef2.jpg";
 import ImageGallery from "../components/ImageGallery";
 import Reviews from "../components/Reviews";
-import chef1 from "../utils/img/chef1.png";
-import chef2 from "../utils/img/chef2.png";
+import chef1 from "../utils/img/chef1.jpg";
+import chef2 from "../utils/img/chef2.jpg";
 import chef3 from "../utils/img/chef3.jpg";
-import chef4 from "../utils/img/chef4.jpg";
+import chef4 from "../utils/img/chef4.png";
 
 const About = () => {
   return (
@@ -68,75 +68,123 @@ const About = () => {
       </div>
 
       {/* team */}
-      <div className="team">
-        <h1>
-          Our <span>Team</span>
-        </h1>
-        <div className="team_box">
-          <div className="profile">
-            <img src={chef1} alt="Chef 1" />
-            <div className="info">
-              <h2 className="name">Chef</h2>
-              <p className="bio">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="team_icon">
-                <i className="fa-brands fa-facebook-f"></i>
-                <i className="fa-brands fa-twitter"></i>
-                <i className="fa-brands fa-instagram"></i>
+
+      <div className="container-xxl pt-5 pb-3">
+        <div className="container">
+          <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h5 className="section-title ff-secondary text-center text-primary fw-normal">
+              Team Members
+            </h5>
+            <h1 className="mb-5">Our Master Chefs</h1>
+          </div>
+          <div className="row g-4">
+            <div
+              className="col-lg-3 col-md-6 wow fadeInUp"
+              data-wow-delay="0.1s"
+            >
+              <div className="team-item text-center rounded overflow-hidden">
+                <div className="rounded-circle overflow-hidden m-4">
+                  <img className="img-fluid" src={chef1} alt="" />
+                </div>
+                <h5 className="mb-0">John Mike</h5>
+                <small>Head Chef</small>
+                <div className="d-flex justify-content-center mt-3">
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="profile">
-            <img src={chef2} alt="Chef 2" />
-            <div className="info">
-              <h2 className="name">Chef</h2>
-              <p className="bio">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="team_icon">
-                <i className="fa-brands fa-facebook-f"></i>
-                <i className="fa-brands fa-twitter"></i>
-                <i className="fa-brands fa-instagram"></i>
+            <div
+              className="col-lg-3 col-md-6 wow fadeInUp"
+              data-wow-delay="0.3s"
+            >
+              <div className="team-item text-center rounded overflow-hidden">
+                <div className="rounded-circle overflow-hidden m-4">
+                  <img className="img-fluid" src={chef2} alt="" />
+                </div>
+                <h5 className="mb-0">Mark</h5>
+                <small>Chef</small>
+                <div className="d-flex justify-content-center mt-3">
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="profile">
-            <img src={chef3} alt="Chef 3" />
-            <div className="info">
-              <h2 className="name">Chef</h2>
-              <p className="bio">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="team_icon">
-                <i className="fa-brands fa-facebook-f"></i>
-                <i className="fa-brands fa-twitter"></i>
-                <i className="fa-brands fa-instagram"></i>
+            <div
+              className="col-lg-3 col-md-6 wow fadeInUp"
+              data-wow-delay="0.5s"
+            >
+              <div className="team-item text-center rounded overflow-hidden">
+                <div className="rounded-circle overflow-hidden m-4">
+                  <img className="img-fluid" src={chef3} alt="" />
+                </div>
+                <h5 className="mb-0">Nike Burn</h5>
+                <small>Chef</small>
+                <div className="d-flex justify-content-center mt-3">
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="profile">
-            <img src={chef4} alt="Chef 4" />
-            <div className="info">
-              <h2 className="name">Chef</h2>
-              <p className="bio">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-              <div className="team_icon">
-                <i className="fa-brands fa-facebook-f"></i>
-                <i className="fa-brands fa-twitter"></i>
-                <i className="fa-brands fa-instagram"></i>
+            <div
+              className="col-lg-3 col-md-6 wow fadeInUp"
+              data-wow-delay="0.7s"
+            >
+              <div className="team-item text-center rounded overflow-hidden">
+                <div className="rounded-circle overflow-hidden m-4">
+                  <img
+                    className="img-fluid"
+                    src={chef4}
+                    alt=""
+                    style={{ height: "215px" }}
+                  />
+                </div>
+                <h5 className="mb-0">Anna Rose</h5>
+                <small>Chef</small>
+                <div className="d-flex justify-content-center mt-3">
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a className="btn btn-square btn-primary mx-1" href="">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <br />
+      <br />
+      <br />
 
       {/* reviews */}
-      <div className="my-5">
+
+      <div className="my-custom-margin">
         <Reviews />
       </div>
     </div>
